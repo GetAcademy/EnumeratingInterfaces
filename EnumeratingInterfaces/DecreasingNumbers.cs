bb7b8a9e677194a05bc7c52c6dbe06824a8872f9
GET Academy <terje@kolderup.net>
2024-06-11 10:34:12 +0200

    Initial commit

---
+++ EnumeratingInterfaces/DecreasingNumbers.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+
+namespace EnumeratingInterfaces
+{
+    internal class DecreasingNumbers 
+        : IEnumerable<int>, IEnumerator<int>
+    {
+        public DecreasingNumbers(int startNumber)
+        {
+            Current = startNumber;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /*
+         *  20 -  9 -  3
+         * 102 - 50 - 24 - 11 - 4 - 1
+         *  50 - 24 - 11 -  4 - 1
+         */
+        public bool MoveNext()
+        {
+            Current = Current / 2 - 1;
+            return Current >= 1;
+        }
+
+        public void Reset()
+        {
+        }
+
+        public int Current { get; private set; }
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ EnumeratingInterfaces/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EnumeratingInterfaces;
+
+Console.WriteLine("Hello, World!");
+
+
+var numbers = new List<int>{1, 2, 3};
+//var numbers = new int[] { 1, 2, 3, 4, 5 };
+//var numbers = new DecreasingNumbers(102);
+
+foreach (var number in numbers)
+{
+    Console.WriteLine(number);
+}
